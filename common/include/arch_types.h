bf8867f32aff99d10d8d88c3c654c26cbc19a9ca
Ingu Kang <igxactly@gmail.com>
2016-07-05 20:34:34 +0900

    WIP for WIP 11

--- common/include/arch_types.h
+++ common/include/arch_types.h
@@ -1,9 +1,12 @@
 #ifndef __ARCH_TYPES_H__
 #define __ARCH_TYPES_H__
 
+#include <gtmr/stdint.h>
+/*
 typedef int int32_t;
 typedef unsigned int uint32_t;
 typedef unsigned short uint16_t;
 typedef unsigned long long uint64_t;
 typedef unsigned char uint8_t;
+*/
 #endif
--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 //#include <gic.h>
 //#include <test/tests.h>
 #include <trap.h>
+#include <log/uart_print.h>
 #include <drivers/sp804_timer.h>
 
 #include <gtmr/limits.h>
-#include <gtmr/stdint.h>
 #include <gtmr/asm.h>
 #include <gtmr/mcrmrc.h>
 #include <gtmr/cp15.h>
@@ -18,47 +18,15 @@ int main()
     int val;
     uart_print(GUEST_LABEL);
     uart_print("\n\r=== Starting platform main\n\r");
-#ifdef TESTS_ENABLE_SP804_TIMER
-    /* Test the SP804 timer */
-    uart_print("%s[%d]\n", __func__, __LINE__);
-    hvmm_tests_sp804_timer();
-    uart_print("%s[%d]\n", __func__, __LINE__);
-#endif
-#ifdef TESTS_TRAP_WFI
-    uart_print("%s[%d]\n", __func__, __LINE__);
-    WFI();
-    uart_print("%s[%d]\n", __func__, __LINE__);
-#endif
-#ifdef TESTS_TRAP_SMC
-    uart_print("%s[%d]\n", __func__, __LINE__);
-    SVC();
-    uart_print("%s[%d]\n", __func__, __LINE__);
-/*    SMC(); */
-#endif
-#ifdef TESTS_TRAP_SCTLR
-    uart_print("%s[%d]\n", __func__, __LINE__);
-    READ_SCTLR(val);
-    uart_print("%s[%d]\n", __func__, __LINE__);
-    WRITE_SCTLR(val);
-    uart_print("%s[%d]\n", __func__, __LINE__);
-#endif
-#ifdef TESTS_TRAP_DDCISW
-    uart_print("%s[%d]\n", __func__, __LINE__);
-    WRITE_DCCISW(val);
-    uart_print("%s[%d]\n", __func__, __LINE__);
-#endif
-#ifdef TESTS_TRAP_ACTLR
-    uart_print("%s[%d]\n", __func__, __LINE__);
-    READ_ACTLR(val);
-    uart_print("%s[%d]\n", __func__, __LINE__);
-    WRITE_ACTLR(val);
-    uart_print("%s[%d]\n", __func__, __LINE__);
-#endif
+
+    while (1) {
+        uart_print("\n\rCNTP_TVAL: ");
+        uart_print_hex32(read_cp32(CNTP_TVAL));
+    }
+
     uart_print("\n\r=== Ending platform main\n\r");
 
-    int i = 0;
-    while (1)
-        uart_print("1");
+    while (1);
 
     return 0;
 }
