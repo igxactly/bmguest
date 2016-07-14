45fb8feba2541897d7afe9b0fba452fd49c7f777
Ingu Kang <igxactly@gmail.com>
2016-07-14 17:07:55 +0900

    check context switch by comparing time

--- main.c
+++ main.c
@@ -13,15 +13,23 @@
 #include <gtmr/vmsa.h>
 #include <gtmr/generic_timer.h>
 
+#define THRESHOLD 500000
+
 int main()
 {
     int val;
     uart_print(GUEST_LABEL);
     uart_print("\n\r=== Starting platform main\n\r");
 
+    uint64_t prev_pct = 0;
+    uint64_t curr_pct = 0;
+    prev_pct = read_cp64(CNTPCT);
     while (1) {
-        uart_print("\n\rCNTP_TVAL: ");
-        uart_print_hex32(read_cp32(CNTP_TVAL));
+        curr_pct = read_cp64(CNTPCT);
+        if ((curr_pct - prev_pct) > THRESHOLD) {
+            uart_print("switch?");
+        }
+        prev_pct = curr_pct;
     }
 
     uart_print("\n\r=== Ending platform main\n\r");
